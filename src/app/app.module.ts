22599e8a3f4e702e5f867a4ba6cbf39985e6cacd
builtolast <justme@codrite.com>
2018-04-09 09:33:00 -0400

    data binding for form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { AnotherComponent } from './another/another.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
-    AnotherComponent
+    AnotherComponent,
+    DatabindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
